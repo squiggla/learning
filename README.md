d9e6d4226ccb43201d3929f48219f2193cb1bb0e
Felix Milewski <felix.milewski@gmail.com>
2023-02-02 21:08:41 +1000

    Bring in the new learning
    around Exercism and scrimba learning

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # Learning
 
-## Run
+## Exercism
+
+```
+exercism download --exercise=hello_world --track=javascript
+cd hello_world
+npm install
+npm test
+npm run watch
+npm test -- --watch
+exercism submit hello_world.js
+```
+
+## Run old node play
 
 ```sh
 cd node_play
---
+++ js_counter/index.js
@@ -0,0 +1,38 @@
+// let humanyears = 14
+// let humanDogRatio = humanyears * 7
+// // console.log(count)
+// console.log(humanDogRatio)
+
+// let myage = 14
+// let humanDogRatio = 7
+// let myDogAge = myage*humanDogRatio
+// console.log(myDogAge)
+
+// let count = 50
+// console.log(count)
+
+// count = count + 50
+// console.log(count)
+
+// count = count - 75
+// console.log(count)
+
+// count = count + 50
+// console.log(count)
+
+//let count = 0
+function increment() {
+   console.log("buttone was clicked")
+}
+function countdown() {
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)    
+}
+
+countdown()
+
+countdown()
+
